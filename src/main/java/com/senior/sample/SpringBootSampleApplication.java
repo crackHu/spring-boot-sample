8ee47f6e02109931c732bdab8bb6cd5dcaa6085e
crackHu <296717367@qq.com>
2017-11-22 14:07:39 +0800

    update

--- src/main/java/com/senior/sample/SpringBootSampleApplication.java
+++ src/main/java/com/senior/sample/SpringBootSampleApplication.java
@@ -2,11 +2,21 @@ package com.senior.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class SpringBootSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello docker";
 	}
 }
