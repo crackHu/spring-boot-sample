136907d5a8f09d6fd6cba8b5fe9cd251fdfcc418
crackHu <296717367@qq.com>
2018-04-01 04:49:10 +0800

    update

--- src/main/java/com/senior/sample/UserController.java
+++ src/main/java/com/senior/sample/UserController.java
@@ -1,8 +1,11 @@
 package com.senior.sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.ConcurrencyFailureException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,11 @@ public class UserController {
 	@GetMapping("/{name}")
 	public String hello(@PathVariable String name) {
 		return "hello " + userService.getUser(name);
+	}
+	
+	@GetMapping("/err")
+	@ResponseStatus(HttpStatus.CONFLICT)
+	public void err() {
+		throw new ConcurrencyFailureException("test err");
 	}
 }
