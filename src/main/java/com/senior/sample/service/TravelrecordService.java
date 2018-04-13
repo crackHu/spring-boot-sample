2d980cbf0c0908e5159512ff3f9095c986033935
crackHu <296717367@qq.com>
2018-04-13 18:04:21 +0800

    update

--- src/main/java/com/senior/sample/service/TravelrecordService.java
+++ src/main/java/com/senior/sample/service/TravelrecordService.java
@@ -1,29 +1,27 @@
 package com.senior.sample.service;
 
-import java.util.List;
-
+import com.senior.sample.domain.Travelrecord;
+import com.senior.sample.repository.TravelrecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.senior.sample.domain.Travelrecord;
-import com.senior.sample.repository.TravelrecordRepository;
+import java.util.List;
 
 @Service
 public class TravelrecordService {
 
-	@Autowired
-	private TravelrecordRepository repository;
+    @Autowired
+    private TravelrecordRepository repository;
 
-	@Transactional(propagation = Propagation.REQUIRED)
-	public Travelrecord save(Travelrecord salesman) {
-		return repository.save(salesman);
-	}
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Travelrecord save(Travelrecord salesman) {
+        return repository.save(salesman);
+    }
 
-	@Transactional(propagation = Propagation.NOT_SUPPORTED)
-	public List<Travelrecord> findAll() {
-		return repository.findAll();
-	}
-
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public List<Travelrecord> findAll() {
+        return repository.findAll();
+    }
 }
